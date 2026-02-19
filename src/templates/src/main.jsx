eb9e7991a2a839a8934c2afd03a0252738e47826
reynal240212 <reynal2402gmail.com>
2026-02-19 11:52:04 -0500

    Serve built frontend from Spring static resources

--- src/templates/src/main.jsx
+++ src/templates/src/main.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateTicket from './pages/CreateTicket';
 import './styles/globals.css';
 
 function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="container">
         <nav className="nav">
-          <a href="/">Tickets</a>
-          <a href="/create">Crear Ticket</a>
+          <Link to="/">Tickets</Link>
+          <Link to="/create">Crear Ticket</Link>
         </nav>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateTicket />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
